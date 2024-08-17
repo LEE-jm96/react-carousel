86b32d69625eea14cd171698702d0c97ba014475
leejungmin <ljm5138@douzone.com>
2024-08-17 13:29:58 +0900

    feat: api type 추가

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,8 +1,10 @@
 import { httpClient } from "./httpClient";
 
+import { dog } from "../types/dog";
+
 export const getDogRandomImg = async () => {
   const apiUrl = "/dog";
-  const { data } = await httpClient.get(apiUrl);
+  const { data } = await httpClient.get<dog>(apiUrl);
 
   return data;
 };
